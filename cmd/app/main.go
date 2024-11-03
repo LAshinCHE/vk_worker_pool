912c45086684eab33625e3cb060b30b91b4dc210
LashinCHE <egor.cherevichin1337@gmail.com>
2024-11-03 03:15:31 +0300

    premitive realistion

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func producer(strs ...string) <-chan string {
+	prod := make(chan string)
+
+	go func() {
+		for _, str := range strs {
+			prod <- str
+		}
+		close(prod)
+	}()
+
+	return prod
+}
+
+func worker(id int, wg *sync.WaitGroup, chanData <-chan string, result chan<- struct{}) {
+	defer wg.Done()
+	for d := range chanData {
+		fmt.Printf("Worker id:%d message: %s\n", id, d)
+		result <- struct{}{}
+	}
+}
+
+const (
+	workerAmount = 3
+)
+
+func main() {
+	wg := &sync.WaitGroup{}
+	resultSig := make(chan struct{})
+
+	wg.Add(workerAmount)
+
+	prod := producer("some data 4", "some data 3", "some data 2", "some data 1", "some data 5")
+
+	for i := 1; i <= workerAmount; i++ {
+		go worker(i, wg, prod, resultSig)
+	}
+	go func() {
+		wg.Wait()
+		close(resultSig)
+	}()
+	for range resultSig {
+	}
 }
--- internal/worker_pool/workerpool.go
+++ internal/worker_pool/workerpool.go
@@ -2,31 +2,27 @@ package workerpool
 
 import (
 	"context"
+	"fmt"
 	"sync"
 )
 
-type Job interface {
-	execute(string) error
-}
+// type Job interface {
+// 	execute() string
+// }
 
 type WorkerPool struct {
 	workersCount int
-	jobs         chan Job
+	jobs         chan string
 	result       chan string
-	ctx          context.Context
-	cancel       context.CancelFunc
-	wg           sync.WaitGroup
+	wg           *sync.WaitGroup
 }
 
-func NewWorkerPool(workerNum int, ctx context.Context) WorkerPool {
-	ctx, cancel := context.WithCancel(ctx)
+func NewWorkerPool(workerNum int) WorkerPool {
 
 	return WorkerPool{
 		workersCount: workerNum,
-		jobs:         make(chan Job),
+		jobs:         make(chan string),
 		result:       make(chan string),
-		ctx:          ctx,
-		cancel:       cancel,
 	}
 }
 
@@ -34,16 +30,37 @@ func NewWorkerPool(workerNum int, ctx context.Context) WorkerPool {
 // Читает сообщение из канала jobs и выводит его на экран и выводит номер нашего воркера
 // так как wp рабоает запускается с одного канала нужно будет реализовать паттерн fanOut для записи задач по всем воркерам
 // считывание будет использоваться при помощи fanIn будет читать данные с воркеров и записывать их в один общий канал result
-func (wp WorkerPool) worker(id int) error {
-	return nil
+func (wp WorkerPool) worker(id int, ctx context.Context) {
+	defer wp.wg.Done() // завершаем работу нашего воркера
+
+	for {
+		select {
+		case job, ok := <-wp.jobs:
+			if !ok {
+				return
+			}
+			fmt.Printf("Worker id: %d message: %s", id, job)
+		case <-ctx.Done():
+			fmt.Println("Context finish")
+			return
+		}
+	}
+	return
+}
+
+func (wp WorkerPool) Run(ctx context.Context) {
+	// for i := 0; i < wp.workersCount; i++{
+
+	// }
+
 }
 
 // Todo:сделать функцию которая будет добавлять нового воркера в наш
-func (wp WorkerPool) AddWorker(numWorkersToAdd int) error {
-	return nil
+func (wp WorkerPool) AddWorker(numWorkersToAdd int) {
+	return
 }
 
 // Todo: Сделать функцию удалеия числа воркеров
-func (wp WorkerPool) DeleteWorker(numWorkersToAdd int) error {
-	return nil
+func (wp WorkerPool) DeleteWorker(numWorkersToAdd int) {
+	return
 }
